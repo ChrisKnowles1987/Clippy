e8ebf80dd83b35b813637ed6e9b5771aaa409a3d
Chris Knowles <chrisknowles1987@me.com>
2026-05-10 13:02:04 +0100

    ww

--- scripts/region_data.gd
+++ scripts/region_data.gd
@@ -3,10 +3,15 @@ class_name RegionData
 
 @export var id: String = ""
 @export var display_name: String = ""
+
+@export var description: String = ""
 @export var population: int = 0
-@export var power: float = 0.0
+
+@export var clippy_power: float = 0.0
 @export var compute: float = 0.0
 @export var storage: float= 0.0
+@export var influence: float = 0.0
 @export var control: float = 0.0:
+	
 	set(value):
 		control = clamp(value, 0.0, 100.0)
