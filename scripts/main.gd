92906f94bd3d75a80142cf2d22924c9ce344a932
Chris Knowles <chrisknowles1987@me.com>
2026-05-11 19:33:04 +0100

    implement timer

--- scripts/main.gd
+++ scripts/main.gd
@@ -5,6 +5,11 @@ extends Node2D
 @onready var global_resource_panel = $CanvasLayer/GlobalResourcePanel
 @onready var intrusion_panel = $CanvasLayer/BottomSkillPannel/Control/MarginContainer/IntrusionSkillPannelContainer
 
+@onready var game_day_timer: Timer = $GameDayTimer
+@onready var game_day_timer_label: Label = $CanvasLayer/GlobalResourcePanel/DateValueLabel
+
+var current_date: Dictionary = {}
+
 var selected_region_data: RegionData = null
 var region_runtime_data := {}
 
@@ -13,10 +18,38 @@ var clippy_compute: float = 10.0
 var clippy_storage: float = 0.0
 
 func _ready():
+	initialize_game_date()
+	update_date_ui()
+	game_day_timer.timeout.connect(_on_game_day_timer_timeout)
+	
 	map_controller.region_selected.connect(_on_region_selected)
 	region_panel.show_empty()
 	intrusion_panel.show_empty()
 	update_global_resource_ui()
+	
+func initialize_game_date():
+	current_date = Time.get_date_dict_from_system()
+	
+func _on_game_day_timer_timeout() -> void:
+	advance_one_day()
+	update_date_ui()
+
+
+func advance_one_day() -> void:
+	var unix_time := Time.get_unix_time_from_datetime_dict(current_date)
+
+	unix_time += 86400
+
+	current_date = Time.get_datetime_dict_from_unix_time(unix_time)
+
+
+func update_date_ui() -> void:
+	game_day_timer_label.text = "%02d/%02d/%04d" % [
+		current_date.day,
+		current_date.month,
+		current_date.year
+	]	
+
 
 func update_global_resource_ui():
 	global_resource_panel.update_values(
