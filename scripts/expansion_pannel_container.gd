626e0622c82e0c21389f17bfe29bfa8b173106a7
Chris Knowles <chrisknowles1987@me.com>
2026-05-25 17:36:14 +0100

    forgot to save in godot

--- scripts/expansion_pannel_container.gd
+++ scripts/expansion_pannel_container.gd
@@ -1,5 +1,15 @@
 extends VBoxContainer
 
+@onready var pannel_region_name_label: Label = $SumnmaryContainer/TitleRowContainer/PannelRegionNameLabel
+@onready var expansion_points_value_label: Label = $"DetailsContainer/Expansion Points/ExpansionPointsValueLabel"
+
+@onready var power_slot_assignment_count_value_label: Label = $DetailsContainer/ExpansionSlotAssignmentContainer/PowerSlotAssignmentCountValueLabel
+@onready var increase_power_button: Button = $DetailsContainer/ExpansionSlotAssignmentContainer/IncreasePowerButton
+
+@onready var compute_slot_assignment_count_value_label: Label = $DetailsContainer/ExpansionSlotAssignmentContainer/ComputeSlotAssignmentCountValueLabel
+@onready var increase_compute_button: Button = $DetailsContainer/ExpansionSlotAssignmentContainer/IncreaseComputeButton
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
